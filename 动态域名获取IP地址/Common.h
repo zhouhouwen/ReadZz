cfc0d7fc8f11f7aa87ae252057989fcb3f8450f3
haven <q724541826@qq.com>
2016-10-19 10:46:27 +0800

    lallaa

---
+++ 动态域名获取IP地址/Common.h
@@ -0,0 +1,13 @@
+//
+//  Common.h
+//  动态域名获取IP地址
+//
+//  Created by 宏电联 on 16/9/26.
+//  Copyright © 2016年 haven.zhw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Common : NSObject
+
+@end
---
+++ 动态域名获取IP地址/Common.m
@@ -0,0 +1,13 @@
+//
+//  Common.m
+//  动态域名获取IP地址
+//
+//  Created by 宏电联 on 16/9/26.
+//  Copyright © 2016年 haven.zhw. All rights reserved.
+//
+
+#import "Common.h"
+
+@implementation Common
+
+@end
